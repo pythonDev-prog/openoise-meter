220aafe2f7d42102ab59cecda36fac70039ce6c0
Mohammed Anam <tamervas@gmail.com>
2026-01-06 23:14:12 +0300

    delte the project

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'it.piemonte.arpa.openoise',
-  appName: 'OpeNoise Meter',
-  webDir: 'www'
-};
-
-export default config;
